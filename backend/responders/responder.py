2b238e4c22bf6b33863b250cf75074884af65276
Lucas McClean <lucksdeveloper@gmail.com>
2026-03-28 18:08:44 -0400

    feat(be): add responder locations

--- backend/responders/responder.py
+++ backend/responders/responder.py
@@ -6,12 +6,30 @@ DATABASE_URL = os.getenv("DATABASE_URL", "uh oh")
 
 engine = create_engine(DATABASE_URL)
 
-def add_responder(id: str,):
+def add_responder(id: str, lat: float, lon: float,):
     with engine.begin() as conn:
         conn.execute(
             text(
-                "INSERT INTO responders (id) "
-                "VALUES (:id)"
+                """
+                    INSERT INTO responders (id, location_geom)
+                    VALUES (
+                        :id,
+                        ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)::geography
+                    )
+                """
             ),
-            {"id": id}
+            {"id": id, "lat": lat, "lon": lon}
+        )
+
+def update_responder(id: int, lat: float, lon: float):
+    with engine.begin() as conn:
+        conn.execute(
+            text(
+                """
+                UPDATE responders
+                SET location_geom = ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)::geography
+                WHERE id = :id
+                """
+            ),
+            {"id": id, "lat": lat, "lon": lon}
         )
